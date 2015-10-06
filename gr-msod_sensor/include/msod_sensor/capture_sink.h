d311a83d6f27618bdcf2fc829471420aa9c20203
M. Ranganathan <mranga@nist.gov>
2015-10-06 09:47:03 -0400

    Integrated capture code with sensor code.

---
+++ gr-msod_sensor/include/msod_sensor/capture_sink.h
@@ -0,0 +1,56 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2015 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+
+#ifndef INCLUDED_CAPTURE_CAPTURE_SINK_H
+#define INCLUDED_CAPTURE_CAPTURE_SINK_H
+
+#include <msod_sensor/api.h>
+#include <gnuradio/sync_block.h>
+
+namespace gr {
+  namespace msod_sensor {
+
+    /*!
+     * \brief <+description of block+>
+     * \ingroup capture
+     *
+     */
+    class MSOD_SENSOR_API capture_sink : virtual public gr::sync_block
+    {
+     public:
+      typedef boost::shared_ptr<capture_sink> sptr;
+
+      /*!
+       * \brief Return a shared_ptr to a new instance of capture::capture_sink.
+       *
+       * To avoid accidental use of raw pointers, capture::capture_sink's
+       * constructor is in a private implementation
+       * class. capture::capture_sink::make is the public interface for
+       * creating new instances.
+       */
+      static sptr make(size_t itemsize, char* capture_dir, char* websocket_url);
+    };
+
+  } // namespace capture
+} // namespace gr
+
+#endif /* INCLUDED_CAPTURE_CAPTURE_SINK_H */
+
