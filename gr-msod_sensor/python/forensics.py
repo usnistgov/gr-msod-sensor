3300a154994150994ea8a2cfda9ba93ce0b402c6
M. Ranganathan <mranga@nist.gov>
2016-04-07 11:49:59 -0400

    Forgot to add dummy forensics wrapper.

---
+++ gr-msod_sensor/python/forensics.py
@@ -0,0 +1,16 @@
+from pymongo import MongoClient
+
+def analyze(algorithm,sensorId,timestamp,host):
+    client = MongoClient("127.0.0.1",33000)
+    db = client.iqcapture.dataMessages
+    query = {"SensorID":sensorId,"t":{"$gte":timestamp}}
+    print query
+    iqsample = db.find_one(query)
+    if iqsample != None:
+       print "foundiqsample"
+    else:
+       print "could not find sample"
+
+    client.close()
+      
+
