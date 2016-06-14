035dafbff6db8d139eee2ebbf7ec1151456d3da5
John Mink <john.mink@nist.gov>
2016-06-14 15:32:22 -0400

    Added calibration block for IQ

--- gr-msod_sensor/cmakeme.sh
+++ gr-msod_sensor/cmakeme.sh
@@ -1,2 +1,2 @@
 rm CMakeCache.txt
-cmake ../ -DCMAKE_EXE_LINKER_FLAGS="-lboost_log -lboost_log_setup -lpthread -lpthread" -DCMAKE_CXX_FLAGS="-DBOOST_LOG_DYN_LINK -std=c++11 -Wno-deprecated" -DMONGO_CLIENT_DIR=/raid/jnm11/mongo-cxx-driver/install -DOPENSSL_INSTALL_DIR=/home/mranga/openssl
+cmake ../ -DCMAKE_EXE_LINKER_FLAGS="-lboost_log -lboost_log_setup -lpthread -lpthread" -DCMAKE_CXX_FLAGS="-DBOOST_LOG_DYN_LINK -std=c++11 -Wno-deprecated" -DMONGO_CLIENT_DIR=$MONGO_CLIENT_DIR -DOPENSSL_INSTALL_DIR=/home/mranga/openssl
