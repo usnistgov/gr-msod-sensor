6862bc4c39f9552c5bfdc3cab13e6e152e24ce38
M. Ranganathan <mranga@nist.gov>
2016-07-26 21:36:04 -0400

    Set processed flag to true.

--- run_usrp.sh
+++ run_usrp.sh
@@ -1,2 +1,3 @@
-python spectrum_monitor_sslsocket_capture_nogui.py -d $MSOD_WEB_HOST --sensorId=NistUSRPSensor1 --mongod-port=33000  --args "addr=usrp4" --source "uhd"   --power-offset  1.7783  --analyze /home/mranga/ltetrigger/gr-ltetrigger/examples/cell_search_file.py --sensorKey=12345 --latitude 39 --longitude -77
+nohup python spectrum_monitor_sslsocket_capture_nogui.py -d $MSOD_WEB_HOST --sensorId=NistUsrpSensor1 --mongod-port=33000  --args "addr=usrp9" --source "uhd"   --power-offset  1.7783  --sensorKey=12345 --latitude 39 --longitude -77&
+disown $*
 
