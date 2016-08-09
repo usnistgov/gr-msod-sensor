5d4c867758267664c7c05246adccf34592c3535f
Douglas Anderson <danderson@its.bldrdoc.gov>
2016-08-09 17:06:02 -0600

    Revert local changes

--- run_mongod.sh
+++ run_mongod.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
 mkdir /tmp/mongod
-nohup mongod -dbpath /tmp/mongod -port 33000 2>&1 >/tmp/mongod.out &
+mongod -dbpath /tmp/mongod -port 33000
--- run_sensor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup python spectrum_monitor_sslsocket_capture_nogui.py -d spectrum.nist.gov --sensorId="NTIACommSensor-1" --mongod-port="33000"  --args "serial=30AD2C7" --source "uhd" --power-offset="0.0344282980111" --gain="30" --lo-offset="0" --sensorKey="30AD2C7" 2>&1 > /tmp/sensor.out &
